5453af2296d7952d23fe8f8a7a0a6d38e83819a0
Balghari <najmbalghari13@gmail.com>
2024-02-06 18:36:59 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+ 
+  return (
+    <div className="bg bg-red-500">
+   <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default App
